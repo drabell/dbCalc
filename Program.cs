c535bddccde4b0133f9356d3c01e7c942ac499a0
Alex Bell <bell@alexanderbell.us>
2026-01-17 21:08:12 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace dbCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double db1 = 56;
+            double db2 = 40;
+            double sum = DecibelCalculator.dbSum(db1, db2);
+            double diff = DecibelCalculator.dbDiff(db1, db2);
+            double ratio = DecibelCalculator.dbRatio(db1, db2);
+
+            Console.WriteLine("Noise Level Calculator");
+            Console.WriteLine("Sum of, db: (" + db1 + ", " + db2 + ") = " + sum.ToString("F1"));
+            Console.WriteLine("Diff of, db: (" + db1 + ", " + db2 + ") = " + diff.ToString("F1"));
+            Console.WriteLine("Ratio of, db: (" + db1 + ", " + db2 + ") = " + ratio.ToString("F1"));
+            Console.WriteLine("-----------------------------------------");
+        }
+    }
+}
---
+++ dbCalc.cs
@@ -0,0 +1,55 @@
+﻿
+namespace dbCalc
+{
+    public static class DecibelCalculator
+    {
+        /// <summary>
+        /// Sum of two sound levels in decibel
+        /// </summary>
+        /// <param name="db1">double</param>
+        /// <param name="db2">double</param>
+        /// <returns>double</returns>
+        public static double dbSum(double db1, double db2)
+        {
+            try
+            {
+                double db2Decimal = Math.Pow(10.0, db1 / 10.0) + Math.Pow(10.0, db2 / 10.0);
+                return 10.0 * Math.Log10(db2Decimal);
+            }
+            catch { throw; }
+        }
+
+        /// <summary>
+        /// Diff of two sound levels in decibel
+        /// </summary>
+        /// <param name="db1">double</param>
+        /// <param name="db2">double</param>
+        /// <returns>double</returns>
+        public static double dbDiff(double db1, double db2)
+        {
+            try
+            {
+                double db2Decimal = Math.Pow(10.0, db1 / 10.0) - Math.Pow(10.0, db2 / 10.0);
+                return 10.0 * Math.Log10(db2Decimal);
+            }
+            catch { throw; }
+        }
+
+        /// <summary>
+        /// Decibel sound levels ratio
+        /// </summary>
+        /// <param name="db1">double</param>
+        /// <param name="db2">double</param>
+        /// <returns>double</returns>
+        public static double dbRatio(double db1, double db2)
+        {
+            try
+            {
+                double dec1 = Math.Pow(10.0, db1 / 10.0);
+                double dec2 = Math.Pow(10.0, db2 / 10.0);
+                return dec1 / dec2; ;
+            }
+            catch { throw; }
+        }
+    }
+}
